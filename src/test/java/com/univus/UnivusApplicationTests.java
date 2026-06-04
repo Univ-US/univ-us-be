a653d103935d07016f57bd6d46b9f89b74a6820f
user <ssangyoungyuwon@gmail.com>
2026-06-04 13:51:41 +0900

    chore: rename test configuration file to application.yml
    - Rename test-application.yml to application.yml in src/test/resources

--- src/test/java/com/univus/UnivusApplicationTests.java
+++ src/test/java/com/univus/UnivusApplicationTests.java
@@ -1,13 +1,14 @@
-package com.univus;
+package com.univus; // 👈 소속 패키지 부활! (경로에 따라 com.univus.app 일 수도 있음)
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:test-application.yml") 
 class UnivusApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
