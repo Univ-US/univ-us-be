ae4db0643448a6df00c3187220c417cd0e5f5488
user <ssangyoungyuwon@gmail.com>
2026-06-04 13:54:51 +0900

    chore: rename test configuration file to application.yml

--- src/test/java/com/univus/UnivusApplicationTests.java
+++ src/test/java/com/univus/UnivusApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource
+@TestPropertySource(locations = "classpath:application-test.yml")
 class UnivusApplicationTests {
 
     @Test
