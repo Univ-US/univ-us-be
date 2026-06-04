81cf1204c02479f3da568e8c824c377a21cf070b
user <ssangyoungyuwon@gmail.com>
2026-06-04 13:57:44 +0900

    chore: rename test configuration file to application.yml

--- src/test/java/com/univus/UnivusApplicationTests.java
+++ src/test/java/com/univus/UnivusApplicationTests.java
@@ -1,4 +1,4 @@
-package com.univus; // 👈 소속 패키지 부활! (경로에 따라 com.univus.app 일 수도 있음)
+package com.univus; 
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
