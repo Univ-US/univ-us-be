18fab9d123be74138f657674236eb2571089a72c
kim-byeonggyu <zenk5554785@gmail.com>
2026-06-05 19:12:56 +0900

    feat: add member logout

---
+++ src/main/java/com/univus/app/member/exception/InvalidLogoutException.java
@@ -0,0 +1,9 @@
+package com.univus.app.member.exception;
+
+public class InvalidLogoutException extends RuntimeException{
+
+    public InvalidLogoutException(String message) {
+        super(message);
+    }
+
+}
